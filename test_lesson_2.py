83042b7cb85625d6b4292c725e36eb3f688215e4
Dmity Kalenov <dmkalyonov@gmail.com>
2023-04-23 17:05:43 +0300

    initial

---
+++ test_lesson_2.py
@@ -0,0 +1,28 @@
+while True:
+    command = input("Выберите операцию: ")
+    if command in "+-*/":
+        break
+    print("Ошибка: такой операции не существует. Попробуйте ещё раз.")
+
+count = 1
+n = int(input(f"Введите число операндов: "))
+number = int(input(f"Введите число {count}: "))
+result_str = str(number)
+result = number
+while n > (1):
+    n -= 1
+    count += 1
+    number = int(input(f"Введите число {count}: "))
+
+    if number != 0:
+        if command == "+":
+            result += number
+        elif command == "-":
+            result -= number
+        elif command == "*":
+            result *= number
+        elif command == "/":
+            result /= number
+    result_str += " " + command + " " + str(number)
+
+print(result_str + " = " + str(result))
